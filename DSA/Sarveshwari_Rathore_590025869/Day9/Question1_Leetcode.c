da61295e559ad2007c5427257aa14e52549d84f9
sarveshwari08 <sarveshwarii8686@gmail.com>
2026-06-23 23:01:34 +0530

    Day 9 Solutions

--- DSA/Sarveshwari_Rathore_590025869/Day9/Question1_Leetcode.c
+++ DSA/Sarveshwari_Rathore_590025869/Day9/Question1_Leetcode.c
@@ -1,10 +1,10 @@
 int lengthOfLastWord(char* s) {
-    int right=strlen(s)-1;
-    int count=0;
-    while(s[right] == ' ' && right>=0){
+    int right = strlen(s) - 1;
+    int count = 0;
+    while(right >= 0 && s[right] == ' ') {
         right--;
     }
-    while(s[right] != ' ' && right>=0){
+    while(right >= 0 && s[right] != ' ') {
         count++;
         right--;
     }
